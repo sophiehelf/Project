a9ec8bd5e2419d27d6d491127e7c5cd369b4f92e
Sophie Helf <sophie.helf@gmail.com>
2017-05-19 15:10:12 -0700

    smallest multiple added

--- even_fibs.rb
+++ even_fibs.rb
@@ -1,10 +1,17 @@
-def even_fibs num 
-	count = 0 
-	fibs = [1, 2]
-	i = 1
-	
-	count
+def smallest_multiple
+  (1..20).reduce(&:lcm)
 end
+
+puts smallest_multiple #answer is 232792560
+
+def smallest_mult #this method is much slower than smallest_multiple method
+  num = 20
+    until (11..20).all?{ |i| num % i == 0 }
+      num += 20
+    end
+  num
+end
+puts smallest_mult
 
 puts even_fibs(10)
 puts even_fibs(4_000_000)
